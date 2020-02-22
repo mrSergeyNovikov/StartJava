e64a226b85fe225253e4af7bec12de0dae333906
mrSergey <you@example.com>
2020-02-22 11:49:53 +0300

    create Wolf  WolfTest

--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -4,15 +4,15 @@ public class WolfTest {
 		Wolf wolf = new Wolf();
 
 		wolf.setName("Хелсинг");
-        System.out.println("Кличка: " + wolf.getName());
-        wolf.setSex("Волк");
-        System.out.println("Пол: " + wolf.getSex());
-        wolf.setAge(10);
-        System.out.println("Возраст: " + wolf.getAge());
-        wolf.setWeight(50.3);
-        System.out.println("Вес: " + wolf.getWeight());
-        wolf.setColor("Черный");
-        System.out.println("Окрас: " + wolf.getColor());
+		System.out.println("Кличка: " + wolf.getName());
+		wolf.setSex("Волк");
+		System.out.println("Пол: " + wolf.getSex());
+		wolf.setAge(10);
+		System.out.println("Возраст: " + wolf.getAge());
+		wolf.setWeight(50.3);
+		System.out.println("Вес: " + wolf.getWeight());
+		wolf.setColor("Черный");
+		System.out.println("Окрас: " + wolf.getColor());
 		
 		wolf.go();
 		wolf.sit();
