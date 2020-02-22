38f4fd4dc8700eb60182678c30af4b9dc82d194c
mrSergey <you@example.com>
2020-02-22 11:48:27 +0300

    create Wolf  WolfTest

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,10 +1,10 @@
 public class Wolf {
 
-	String name;
-	String sex;
-	int age;
-	double weight;
-	String color;
+	private String name;
+	private String sex;
+	private int age;
+	private double weight;
+	private String color;
 
 	public void go() {
 	}
@@ -19,5 +19,49 @@ public class Wolf {
 	}
 
 	public void hunt() {
+	}
+
+	// сеттеры и геттеры
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+		if (age > 8) {
+			System.out.println("Некорректный возраст");
+		}
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
 	}
 }
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -3,16 +3,16 @@ public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolf = new Wolf();
 
-		wolf.name = "Хелсинг";
-		System.out.println("Кличка: " + wolf.name);
-		wolf.sex = "Волк";
-		System.out.println("Пол: " + wolf.sex);
-		wolf.age = 10;
-		System.out.println("Возраст: " + wolf.age);
-		wolf.weight = 50.3;
-		System.out.println("Вес: " + wolf.weight);
-		wolf.color = "Черный";
-		System.out.println("Окрас: " + wolf.color);
+		wolf.setName("Хелсинг");
+        System.out.println("Кличка: " + wolf.getName());
+        wolf.setSex("Волк");
+        System.out.println("Пол: " + wolf.getSex());
+        wolf.setAge(10);
+        System.out.println("Возраст: " + wolf.getAge());
+        wolf.setWeight(50.3);
+        System.out.println("Вес: " + wolf.getWeight());
+        wolf.setColor("Черный");
+        System.out.println("Окрас: " + wolf.getColor());
 		
 		wolf.go();
 		wolf.sit();
