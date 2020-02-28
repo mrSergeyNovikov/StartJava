c11ea6d1cf5f762ada3cbd8e805d536601e88e66
mrSergey <you@example.com>
2020-02-28 11:15:27 +0300

    change GuessNumber

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class GuessNumber {
-	private int guessNumber = (int)(Math.random() * 100) + 1;
+	
 	Private Scanner scan = new Scanner(System.in);
 	private Player player1;
 	private Player player2;
@@ -11,22 +11,16 @@ public class GuessNumber {
 		this.player2 = player2;
 	}
 
-	public String getNamePlayers() {
-		System.out.println("Введите имя первого игрока" + player1);
-		String enteredName = scan.next();
-		System.out.println("Введите имя второго игрока" + player2);
-		String enteredName2 = scan.next();
-		return enteredName;
-	}
-
 	public void enteredNumbersPlayers(Player player) {
 		System.out.println(player.getName() +"Введите число: ");
+		player.setNumber(scan.nextInt());
 	}
 
 	public void checkEnteredNumber(Player player) {
 		System.out.println("* число для угадывания * " +guessNumber);
 		if (player.getNumber() == guessNumber) {
 			System.out.println("Победитель!!!");
+			int guessNumber = (int)(Math.random() * 100) + 1;
 		} else if (player.getNumber() < guessNumber) {
 			System.out.println("Введеное вами число меньше задуманного");
 		} else if (player.getNumber() > guessNumber) {
--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -6,6 +6,9 @@ public class GuessNumberTest {
 		Scanner scan = new Scanner(System.in);
 		String answerOnQuestion = "да";
 
+		System.out.println("Игра. Угадай чило от 0 до 100...");
+		System.out.println();
+
 		System.out.println("Игрок " + 1 + " введите имя: ");
 		Player player1 = new Player(scan.next());
 		System.out.println("Игрок " + 2 + " введите имя: ");
@@ -13,7 +16,12 @@ public class GuessNumberTest {
 
 		do {
 
-		} while (answerOnQuestion.equals("да"));
-		System.out.println("Пока!");
+
+			do {
+				System.out.print("Хотите продолжить? [да/нет]: ");
+				answerOnQuestion = scan.next();
+			} while (!answerOnQuestion.equalsIgnoreCase("да") && !answerOnQuestion.equalsIgnoreCase("нет"));
+		} while (answerOnQuestion.equalsIgnoreCase("да"));
+		System.out.println("Пока! ");
 	}
 }
